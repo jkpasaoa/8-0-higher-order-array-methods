44961994092bc02483999a58eee2c01bf1179b16
jkpasaoa <jacqueline.pasaoa@gmail.com>
2022-11-20 17:37:45 -0500

    some=every.js; done

--- src/04-find.js
+++ src/04-find.js
@@ -25,14 +25,17 @@ function findPinkElephantsByTimestreet(songs) {
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object} A single object.
  */
-function findFirstSongUnderThreeMinutes(songs) {}
+function findFirstSongUnderThreeMinutes(songs) {
+}
 
 /**
  * Returns the first song in the list where the song title equals the song album.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object} A single object.
  */
-function findFirstTitleTrack(songs) {}
+function findFirstTitleTrack(songs) {
+
+}
 
 module.exports = {
   findPinkElephantsByTimestreet,
